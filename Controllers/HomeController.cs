bd5ea2b794c73f96ecb5de13da80f034e1631b52
Toba Shaukat <toba@thetasolutions.co.uk>
2021-11-12 17:17:59 +0500

    Lecture 9 > IList , Foreach , Bootstrap Table

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,17 +24,56 @@ namespace DotNet5OctBatch_2021.Controllers
             oUser1.Id = 1;
             oUser1.Name = "ABC";
             oUser1.Password = "TEST";
-            ViewBag.User1 = oUser1;
+            //  ViewBag.User1 = oUser1;
 
-            Users oUser2 = new Users();
-            oUser2.Id = 2;
-            oUser2.Name = "XYZ";
-            oUser2.Password = "123";
+            Users oUser2 = new Users
+            {
+                Id = 2,
+                Name = "XYZ",
+                Password = "123"
+            };
 
-            ViewBag.OUser2 = oUser2;
+            Users oUser3 = new Users
+            {
+                Id = 3,
+                Name = "XYZ-ABC",
+                Password = "1234556"
+            };
+           // ViewBag.OUser2 = oUser2;
+            IList<Users> oListUsers = new List<Users>();
+            oListUsers.Add(oUser1);
+            oListUsers.Add(oUser2);
+            oListUsers.Add(oUser3);
+            ViewBag.UsersList = oListUsers;
             return View();
         }
 
+        public IActionResult UsersList()
+        {
+            Users oUser1 = new Users();
+            oUser1.Id = 1;
+            oUser1.Name = "ABC";
+            oUser1.Password = "TEST";
+            Users oUser2 = new Users
+            {
+                Id = 2,
+                Name = "XYZ",
+                Password = "123"
+            };
+            Users oUser3 = new Users
+            {
+                Id = 3,
+                Name = "XYZ-ABC",
+                Password = "1234556"
+            };
+            IList<Users> oListUsers = new List<Users>();
+            oListUsers.Add(oUser3);
+            oListUsers.Add(oUser1);
+            oListUsers.Add(oUser2);
+       
+            ViewBag.UsersList = oListUsers;
+            return View();
+        }
         public IActionResult MyContact()
         {
             ViewBag.Name = "ASP Dot Net";
